61152f0dcd59477a953dd1b9d6b2b3e7d4edad7b
chmodmasx <daninicob@gmail.com>
2024-10-09 02:09:21 -0300

    Update enviar_a_telegram.sh

--- enviar_a_telegram.sh
+++ enviar_a_telegram.sh
@@ -10,17 +10,18 @@ echo "En Dios confiamos | In God we trust"
 echo "\n"
 
 # Contenido para enviar_a_telegram.nemo_action
-NEMO_ACTION_CONTENT="[Nemo Action]\nName=Enviar a Telegram\nName[pl]=Wyślij przez Telegram\nComment=Envía archivos a Telegram desde el menú contextual\nComment[pl]=Wysyła ten plik na czat w Telegramie\nQuote=double\nExec=/usr/share/nemo/actions/enviar_a_telegram.sh %F\nIcon-Name=org.telegram.desktop\nSelection=NotNone\nExtensions=nodirs;\nDependencies=telegram-desktop"
+NEMO_ACTION_CONTENT="[Nemo Action]\nName=Enviar a Telegram\nName[pl]=Wyślij przez Telegram\nComment=Envía archivos a Telegram desde el menú contextual\nComment[pl]=Wysyła ten plik na czat w Telegramie\nQuote=double\nExec=/home/$USER/.local/share/nemo/actions/enviar-a-telegram@chmodmasx/enviar_a_telegram.sh %F\nIcon-Name=org.telegram.desktop\nSelection=NotNone\nExtensions=nodirs;\nDependencies=telegram-desktop"
 
 # Contenido para enviar_a_telegram.sh
 SEND_SCRIPT_CONTENT='#!/usr/bin/env bash\n\narr=()\nSAVEIFS=$IFS\nIFS=$(echo -en "\n\b")\n\nfor var in "$@"\ndo\n    path=$(readlink -f $var)\n    arr+=($path)\ndone\n\n# Check if the client is a native app\nif [ $(command -v telegram-desktop) ]; then\n    telegram-desktop -sendpath "${arr[@]}"\n    wmctrl -x -a Telegram.TelegramDesktop\nelse\n    flatpak run --file-forwarding org.telegram.desktop -sendpath @@ "${arr[@]}" @@\n    wmctrl -x -a Telegram\nfi\n\nIFS=$SAVEIFS'
 
 # Ruta donde se crearán los archivos
+mkdir -p "/home/$USER/.local/share/nemo/actions/enviar-a-telegram@chmodmasx"
 DESTINATION_DIR="/home/$USER/.local/share/nemo/actions"
 
 # Rutas completas para los archivos
 NEMO_ACTION_FILE="$DESTINATION_DIR/enviar_a_telegram.nemo_action"
-SEND_SCRIPT_FILE="$DESTINATION_DIR/enviar_a_telegram.sh"
+SEND_SCRIPT_FILE="$DESTINATION_DIR/enviar-a-telegram@chmodmasx/enviar_a_telegram.sh"
 
 # Verificar si el usuario tiene permisos de escritura en la carpeta de destino
 if [ ! -w "$DESTINATION_DIR" ]; then
