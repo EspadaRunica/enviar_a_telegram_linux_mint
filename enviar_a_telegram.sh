49d5f936820cd80ee350a2ff22f995ad9809f5b7
chmodmasx <daninicob@gmail.com>
2024-10-09 02:35:02 -0300

    Update enviar_a_telegram.sh

--- enviar_a_telegram.sh
+++ enviar_a_telegram.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-echo "   ____       _                         _                          ";
-echo "  / __ \  ___| |__  _ __ ___   ___   __| |_ __ ___   __ _ _____  __";
-echo " / / _\` |/ __| '_ \| '_ \` _ \ / _ \ / _\` | '_ \` _ \ / _\` / __\ \/ /";
-echo "| | (_| | (__| | | | | | | | | (_) | (_| | | | | | | (_| \__ \>  < ";
-echo " \ \__,_|\___|_| |_|_| |_| |_|\___/ \__,_|_| |_| |_|\__,_|___/_/\_\ ";
-echo "  \____/                                                           ";
+echo "   ____       _                         _                          "
+echo "  / __ \  ___| |__  _ __ ___   ___   __| |_ __ ___   __ _ _____  __"
+echo " / / _\` |/ __| '_ \| '_ \` _ \ / _ \ / _\` | '_ \` _ \ / _\` / __\ \/ /"
+echo "| | (_| | (__| | | | | | | | | (_) | (_| | | | | | | (_| \__ \>  < "
+echo " \ \__,_|\___|_| |_|_| |_| |_|\___/ \__,_|_| |_| |_|\__,_|___/_/\_\ "
+echo "  \____/                                                           "
 echo "En Dios confiamos | In God we trust"
-echo "\n"
+echo ""
 
-# Contenido para enviar_a_telegram.nemo_action
-NEMO_ACTION_CONTENT="[Nemo Action]\nName=Enviar a Telegram\nName[pl]=Wyślij przez Telegram\nComment=Envía archivos a Telegram desde el menú contextual\nComment[pl]=Wysyła ten plik na czat w Telegramie\nQuote=double\nExec=/home/$USER/.local/share/nemo/actions/enviar-a-telegram@chmodmasx/enviar_a_telegram.sh %F\nIcon-Name=org.telegram.desktop\nSelection=NotNone\nExtensions=nodirs;\nDependencies=telegram-desktop"
-
-# Contenido para enviar_a_telegram.sh
-SEND_SCRIPT_CONTENT='#!/usr/bin/env bash\n\narr=()\nSAVEIFS=$IFS\nIFS=$(echo -en "\n\b")\n\nfor var in "$@"\ndo\n    path=$(readlink -f $var)\n    arr+=($path)\ndone\n\n# Check if the client is a native app\nif [ $(command -v telegram-desktop) ]; then\n    telegram-desktop -sendpath "${arr[@]}"\n    wmctrl -x -a Telegram.TelegramDesktop\nelse\n    flatpak run --file-forwarding org.telegram.desktop -sendpath @@ "${arr[@]}" @@\n    wmctrl -x -a Telegram\nfi\n\nIFS=$SAVEIFS'
 
 # Ruta donde se crearán los archivos
 mkdir -p "/home/$USER/.local/share/nemo/actions/enviar-a-telegram@chmodmasx"
@@ -21,7 +16,7 @@ DESTINATION_DIR="/home/$USER/.local/share/nemo/actions"
 
 # Rutas completas para los archivos
 NEMO_ACTION_FILE="$DESTINATION_DIR/enviar_a_telegram.nemo_action"
-SEND_SCRIPT_FILE="$DESTINATION_DIR/enviar-a-telegram@chmodmasx/enviar_a_telegram.sh"
+SEND_SCRIPT_FILE="/home/$USER/.local/share/nemo/actions/enviar-a-telegram@chmodmasx/enviar_a_telegram.sh"
 
 # Verificar si el usuario tiene permisos de escritura en la carpeta de destino
 if [ ! -w "$DESTINATION_DIR" ]; then
@@ -30,12 +25,53 @@ if [ ! -w "$DESTINATION_DIR" ]; then
 fi
 
 # Crear el archivo enviar_a_telegram.nemo_action
-echo -e "$NEMO_ACTION_CONTENT" > "$NEMO_ACTION_FILE"
+cat <<EOF > "$NEMO_ACTION_FILE"
+[Nemo Action]
+Name=Enviar a Telegram
+Name[pl]=Wyślij przez Telegram
+Comment=Envía archivos a Telegram desde el menú contextual
+Comment[pl]=Wysyła ten plik na czat w Telegramie
+Quote=double
+Exec=/home/$USER/.local/share/nemo/actions/enviar-a-telegram@chmodmasx/enviar_a_telegram.sh %F
+Icon-Name=org.telegram.desktop
+Selection=NotNone
+Extensions=nodirs;
+EOF
+
+# Crear el archivo enviar_a_telegram.sh
+cat <<EOF > "$SEND_SCRIPT_FILE"
+#!/usr/bin/env bash
+
+arr=()
+SAVEIFS=\$IFS
+IFS=\$(echo -en "\n\b")
+
+for var in "\$@"
+do
+    # Verificar si la variable no está vacía
+    if [ -n "\$var" ]; then
+        path=\$(readlink -f "\$var")
+        arr+=("\$path")
+    fi
+done
+
+# Check if the client is a native app
+if command -v telegram-desktop > /dev/null; then
+    telegram-desktop -sendpath "\${arr[@]}"
+    wmctrl -x -a Telegram.TelegramDesktop
+elif command -v telegram > /dev/null; then
+    telegram -sendpath "\${arr[@]}"
+    wmctrl -x -a Telegram.TelegramDesktop
+else
+    flatpak run --file-forwarding org.telegram.desktop -sendpath @@ "\${arr[@]}" @@
+    wmctrl -x -a Telegram
+fi
+
+IFS=\$SAVEIFS
+EOF
 
 # Dar permisos de ejecución al archivo enviar_a_telegram.sh
-echo -e "$SEND_SCRIPT_CONTENT" > "$SEND_SCRIPT_FILE"
 chmod +x "$SEND_SCRIPT_FILE"
 
 # Mensaje de confirmación
 echo "Archivos creados exitosamente en: $DESTINATION_DIR"
-
